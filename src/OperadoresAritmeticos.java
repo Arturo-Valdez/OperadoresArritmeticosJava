38880c397700dd35efda138bcc40ee6a00497a6c
Arturo Valdez <santiguardado27@gmail.com>
2024-12-29 15:51:46 -0600

    Operadores Aritmeticos + - * / %

--- src/OperadoresAritmeticos.java
+++ src/OperadoresAritmeticos.java
@@ -1,5 +1,27 @@
 public class OperadoresAritmeticos {
     public static void main(String[] args) {
         System.out.println("*** Operadores Aritmeticos ***");
+        double a =5, b = 3, resultado;
+        
+        //Suma
+        resultado = a + b;
+        System.out.println("Resultado Suma= " + resultado);
+
+        //Resta
+        resultado = a - b;
+        System.out.println("Resultado Resta = " + resultado);
+
+        //Multiplicacion
+        resultado = a * b;
+        System.out.println("Resultado Multiplicacion = " + resultado);
+
+        //Divicion
+        resultado = a / b;
+        System.out.println("Resultado Divicion = " + resultado);
+
+        //Modulo ( Residuo division) %
+        resultado = a % b;
+        System.out.println("Resultado = " + resultado);
+
     }
 }
