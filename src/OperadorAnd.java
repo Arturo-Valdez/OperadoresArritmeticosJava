db87ecde898d31f732d42d6afe03af9ec1c5edbe
Arturo Valdez <santiguardado27@gmail.com>
2024-12-30 19:39:14 -0600

    Operadore AND

---
+++ src/OperadorAnd.java
@@ -0,0 +1,11 @@
+public class OperadorAnd {
+    public static void main(String[] args) {
+        System.out.println("*** Operador AND ***");
+
+        boolean a = true, b = true;
+        //AND ( regresa true si ambos valores son true)
+        var resultado = a && b;
+        System.out.println("resultado a && b = " + resultado);
+    }
+
+}
