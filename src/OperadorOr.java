6a35d8a2bc478cb0e61fe3f9a7951a96f17a86f2
Arturo Valdez <santiguardado27@gmail.com>
2024-12-30 19:42:40 -0600

    Operadore OR

---
+++ src/OperadorOr.java
@@ -0,0 +1,10 @@
+public class OperadorOr {
+    public static void main(String[] args) {
+        System.out.println("*** Operador OR");
+        // OR (Regresa true si cualquiera de los valores es true
+
+        boolean a = false, b = false;
+        var resultado = (a || b); // operacion caracter pipe ||
+        System.out.println("resultado a || b = " + resultado);
+    }
+}
