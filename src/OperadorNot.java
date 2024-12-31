7af78b2c18965798c0d8690c98a94b370540a36e
Arturo Valdez <santiguardado27@gmail.com>
2024-12-30 19:44:51 -0600

    Operadore NOT

---
+++ src/OperadorNot.java
@@ -0,0 +1,11 @@
+public class OperadorNot {
+    public static void main(String[] args) {
+        System.out.println("*** Operador NOT ***");
+
+        //Operador NOT ! (invierte el valor logico)
+
+        boolean a = false;
+        var resultado = !a;
+        System.out.println("resultado !a = " + resultado);
+    }
+}
